5dcbfa0511bb31d919c3559660c40363b67ffee1
지운 <byun91@semyung.ac.kr>
2021-10-28 00:43:32 +0900

    배달앱 메인액티비티 구축

--- app/src/main/java/byun91/delivery/screen/main/MainActivity.kt
+++ app/src/main/java/byun91/delivery/screen/main/MainActivity.kt
@@ -2,11 +2,32 @@ package byun91.delivery.screen.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import byun91.delivery.R
+import byun91.delivery.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        bottomNav.setOnItemSelectedListener(this@MainActivity)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean
+    = when(item.itemId){
+        R.id.menu_home ->{
+            true
+        }
+        R.id.menu_my ->{
+            true
+        } else -> false
     }
 }
